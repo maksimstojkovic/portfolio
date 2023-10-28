6c0b7114de4d4a81ccfb189c385831513182b5d0
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-10-29 03:52:14 +1100

    Add svg icons

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import "./style.scss";
+import "./img/cellphone.svg";
+import "./img/email.svg";
+import "./img/github.svg";
+import "./img/linkedin.svg";
+import "./img/newspaper-variant.svg";
+import "./img/open-in-new.svg";
