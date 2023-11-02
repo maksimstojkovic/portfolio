41d31412fa726b93dfc94d06a5bc959fb2b535a0
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-11-02 23:23:58 +1100

    Add project images

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "./style.scss";
+
 import "./img/cellphone.svg";
 import "./img/email.svg";
 import "./img/github.svg";
@@ -6,4 +7,13 @@ import "./img/linkedin.svg";
 import "./img/newspaper-variant.svg";
 import "./img/open-in-new.svg";
 import "./img/script-text.svg"
-import "./img/p2p_network.png"
+
+import "./img/p2p_energy_trading.png"
+import "./img/battleship.png"
+import "./img/calculator.png"
+import "./img/tictactoe.png"
+import "./img/restaurant.png"
+import "./img/letsencrypt.png"
+import "./img/duckdns.png"
+import "./img/sitz.png"
+
